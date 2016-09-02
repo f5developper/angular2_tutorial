10d3f7bbe2dfdc46844717cdd1c5d1023d0f6dca
int_adachi <int_adachi@sakura.ad.jp>
2016-09-02 18:33:10 +0900

    main.tsまで

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,18 +1,18 @@
 /**
-* アングラーは使用するモジュールをimport文で定義するけど、定義した中で更に一つを指定する
-* 必要がある。
-**/
+ * アングラーは使用するモジュールをimport文で定義するけど、定義した中で更に一つを指定する
+ * 必要がある。
+ **/
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-	//他のモジュールを使用したい場合に使う。
-	//rootのモジュールはこのbrowserModuleを読み込んだ方が良いらしい
-	//importsに渡しているBrowserModuleは配列で渡します。
-	//このアングラーのモジュールはブラウザでも動作するアプリケーションであるということです。
-	//要はブラウザでも動くし、バッチとかでも動くよみたいな複数の当て込みができるということっぽい。
+    //他のモジュールを使用したい場合に使う。
+    //rootのモジュールはこのbrowserModuleを読み込んだ方が良いらしい
+    //importsに渡しているBrowserModuleは配列で渡します。
+    //このアングラーのモジュールはブラウザでも動作するアプリケーションであるということです。
+    //要はブラウザでも動くし、バッチとかでも動くよみたいな複数の当て込みができるということっぽい。
     imports: [BrowserModule],
     //このモジュールがどのコンポーネントを使用するかを指示する
     declarations: [AppComponent],
